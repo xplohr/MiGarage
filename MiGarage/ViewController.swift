3f8a95ea66707d30b5aee36ee28dad4dcef66f09
Che-Chuen Ho <che@cartsnap.com>
2015-08-16 15:19:16 -0700

    Initial Commit

---
+++ MiGarage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiGarage
+//
+//  Created by Che-Chuen Ho on 8/16/15.
+//  Copyright (c) 2015 Che-Chuen Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
